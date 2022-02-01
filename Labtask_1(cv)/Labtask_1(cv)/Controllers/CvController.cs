4c13b98c74f00e66421cb6ea9d1dd612fa2d67e0
didipta <66532706+didipta@users.noreply.github.com>
2022-02-01 17:40:40 +0600

    Initial commit

---
+++ Labtask_1(cv)/Labtask_1(cv)/Controllers/CvController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Labtask_1_cv_.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: Cv
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
---
+++ Labtask_1(cv)/Labtask_1(cv)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Labtask_1_cv_.MvcApplication" Language="C#" %>
